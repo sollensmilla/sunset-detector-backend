ce8561e2bcf03d6cf594996c931900576cd0a3c4
Smilla Sollén <ss226uk@student.lnu.se>
2026-05-21 21:02:23 +0200

    Use cors

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 import express from 'express'
+import cors from 'cors'
 
 import dataRoutes from './routes/dataRoutes.js'
 
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 app.use('/api', dataRoutes)
 
